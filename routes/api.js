7bd77ea475fb319a53ecbb7c5ed4d5c72071ff63
Albert Cervantes <cervanalbert@gmail.com>
2020-04-29 03:59:44 -0700

    added models to be used

--- routes/api.js
+++ routes/api.js
@@ -1,33 +1,34 @@
 const router = require("express").Router();
 
 // TODO: import required model/s
+const db = require("./models");
 
 // TODO: and add code to the routes so that the app functions correctly
 
 // Creates a workout using data in the request body.
 router.post("/api/workouts", (req, res) => {
-  // CODE HERE
+    // CODE HERE
 });
 
 // Respond with workout for id url parameter. This should
 // respond with the updated workout json
 router.put("/api/workouts/:id", (req, res) => {
-  // CODE HERE
+    // CODE HERE
 });
 
 // Respond with json for all the workouts in an array.
 router.get("/api/workouts", (req, res) => {
-  // CODE HERE
+    // CODE HERE
 });
 
 // Respond with json array containing the last 7 workouts
 router.get("/api/workouts/range", (req, res) => {
-  // CODE HERE
+    // CODE HERE
 });
 
 // Delete workout with id matching id in the request body.
 router.delete("/api/workouts", (req, res) => {
-  // CODE HERE
+    // CODE HERE
 });
 
 module.exports = router;
