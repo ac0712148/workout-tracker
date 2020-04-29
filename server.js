9cfd5137ddb60f22ded3e4669c5eafb21239447d
Albert Cervantes <cervanalbert@gmail.com>
2020-04-28 19:35:41 -0700

    added server with mongoose

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const logger = require("morgan");
+
+// import mongoose
+
+const mongoose = require("mongoose");
+
+const apiRoutes = require("./routes/api");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// use logging middleware
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Create mongoose connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/trackerdb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+app.use(apiRoutes);
+
+app.listen(PORT, () => {
+    console.log(`App running on port: http://localhost:${PORT}`);
+})
