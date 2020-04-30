f45c6cc3144a1812ae7d34069dea841036488ff5
Albert Cervantes <cervanalbert@gmail.com>
2020-04-30 15:24:56 -0700

    added total duration virtual method

--- models/workout.js
+++ models/workout.js
@@ -5,13 +5,13 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
     day: {
         type: Date,
-        required: true
+        default: Date.now
     },
     exercises: [{
         type: {
             type: String,
             trim: true,
-            required: "Select a type"
+            required: "Select a valid exercise type"
         },
         name: {
             type: String,
@@ -35,6 +35,17 @@ const WorkoutSchema = new Schema({
             type: Number
         },
     }]
+}, {
+    toJSON: {
+        virtuals: true
+    }
+});
+
+WorkoutSchema.virtual("totalDuration").get(function() {
+    return this.exercises.reduce((total, current) => {
+        total += current.duration;
+        return total;
+    }, 0);
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
